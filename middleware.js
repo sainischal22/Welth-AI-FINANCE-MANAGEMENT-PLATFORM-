5cc8562f7748e920d724d783202923bb5023e02f
sainischal22 <165552239+sainischal22@users.noreply.github.com>
2025-09-25 18:36:05 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,36 +2,22 @@ import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
-
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
-    shield({
-      mode: "LIVE",
-    }),
+    shield({ mode: "LIVE" }),
     detectBot({
       mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE",
-        "GO_HTTP",
-      ],
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
     }),
   ],
 });
 
 const clerk = clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
-  if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
+  const { userId, redirectToSignIn } = await auth();
+  if (!userId && createRouteMatcher(["/dashboard(.*)", "/account(.*)", "/transaction(.*)"])(req)) {
     return redirectToSignIn();
   }
-
   return NextResponse.next();
 });
 
@@ -43,4 +29,3 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
-
